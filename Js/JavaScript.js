f96bfd9857f86e17399d88356af0b52c42dd948f
hoangvupcx <89677053+hoangvupcx@users.noreply.github.com>
2021-10-04 11:17:21 +0700

    Code

--- Js/JavaScript.js
+++ Js/JavaScript.js
@@ -2,23 +2,22 @@ $(document).ready(function() {
 
     $('#toggle').click(function() {
         $('nav > ul.menu').slideToggle()
-        // $('.accordion').hide("slow")
-    })
+    }) // Thả thanh navbar
 
     $('.search-toggle').click(function() {
         $('.search-box').slideToggle()
-    })
+    }) // Thả hộp thoại tìm kiếm
 
     $('.login-and-register').click(function() {
         $('.login-register').slideToggle()
-    })
+    }) // Thả selection register & login
 
-    $("body .body-product:first-child").addClass("wow animate__fadeInLeft")
-    $("body .body-product:last-child").addClass("wow animate__fadeInRight")
-    $("body .body-product:nth-child(2)").addClass("wow animate__fadeInUp")
-    $("body .product-item:nth-child(even) > div").addClass("wow animate__fadeInRight")
-    $("body .product-item:nth-child(odd) > div").addClass("wow animate__fadeInLeft")
-    $("body ul.menu li a").addClass("wow animate__backInUp")
+    $("body .body-product:first-child").addClass("wow animation-left")
+    $("body .body-product:last-child").addClass("wow animation-right")
+    $("body .body-product:nth-child(2)").addClass("wow animate__fadeIn animation-inUp")
+    $("body .product-item:nth-child(even) > div").addClass("wow animation-right")
+    $("body .product-item:nth-child(odd) > div").addClass("wow animation-left")
+    $("body ul.menu li a").addClass("wow animation-backInUp")
     
     wow = new WOW(
         {
@@ -28,18 +27,8 @@ $(document).ready(function() {
             mobile:       true,       // default
             live:         true        // default
         })
-    wow.init();
+    wow.init(); // Hiệu ứng chuyển động
 
-      
-
-    /*$(window).scroll(function() {
-        if ($(this).scrollTop() >= $(".picture-product img:last-child").prop('offsetTop') || $(this).scrollTop() <= 0)
-        $('.accordion').hide("slow")
-        else if ($(this).scrollTop() >= $(".menu-details").prop('offsetTop'))
-        $('.accordion').show("slow")
-        else 
-        $('.accordion').hide("slow")
-    })*/
 
     $(window).scroll(function(event) {
         var pos_body = $('html,body').scrollTop();
@@ -49,28 +38,27 @@ $(document).ready(function() {
         else{
             $('.gotop').removeClass('hien-ra');
         }
-    });
+    }); // Xử lý khi button-go-to-top di chuyển lên gần đầu trang
 
     $('.gotop').click(function(event) {
         $('html,body').animate({scrollTop: 0},800);
-    });
+    }); // Button go to top
 
     $('.gobot').click(function(event) {
         $('html,body').animate({scrollTop: $(".Detail-product").prop('offsetTop')},800);
-    });
+    }); // Button di chuyển xuống details products
 
-    $('.accordion-item1.active .accordion-body1').slideDown();
+    $('.accordion-item1.active .accordion-body1').slideDown(); // Khi accordion active thì đổi màu nền 
     $('.accordion-header1').click(function(){
         $(this).parent().toggleClass('active');
         $(this).parent().children('.accordion-body1').slideToggle(); // Từ header tìm về cha (item) rồi đưa về con (body)
-    })
+    }) // Thả accordion xuống (responsive)
 
     $('#btnSearch').click(function() {
         var k = $('#kw').val()
         $(`div.text-product p:contains(${k})`).parent().parent().css("background-color", "#d5d3cd")
         $(`div.text-product pre:contains(${k})`).parent().parent().css("background-color", "#d5d3cd")
-    })
+    }) // Công cụ tìm kiếm
     
-
 });
 
